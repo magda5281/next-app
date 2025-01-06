07a70caba9443ac8343bf0ac59c97a9c36bad31f
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-06 20:00:25 +0000

    Private Route

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PrivateRoute() {
+ return (
+  <h2>
+   You can not view this in the browser as the folder is named with underscore
+   making that route private
+  </h2>
+ );
+}
