5a7cb1e0d21b768a8de73684f611eec71f99ced0
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-06 19:48:29 +0000

    not-fund route

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Notfound() {
+ return <h2>Review not found </h2>;
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { notFound } from 'next/navigation';
 export default function ReviewDetail({
  params,
 }: {
  params: { productId: string; reviewId: string };
 }) {
+ if (parseInt(params.reviewId) > 1000) {
+  notFound();
+ }
  return (
   <h3>
    Review {params.reviewId} for product {params.productId}
