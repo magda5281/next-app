c89e53969f50334b2149ef2b916966b3f91d4896
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-06 19:53:57 +0000

    Colocating components

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LineChart() {
+ return <h2>Line chart</h2>;
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BarChart() {
+ return <div>Bar chart</div>;
+}
+
+export default function Dashboard() {
+ return <h2>Dashboard</h2>;
+}
