0c9a4dccc8b3c954cd5f251ed370a95a4bd002c5
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-07 15:02:18 +0000

    Creating navigation with Link component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,27 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Products() {
+ const productId = 100;
  return (
   <>
+   <Link href='/'>Home</Link>
    <h2>Product list</h2>
-   <h3>Product 1</h3>
-   <h3>Product 2</h3>
-   <h3>Product 3</h3>
+   <h3>
+    <Link href={'/products/1'}>Product 1</Link>
+   </h3>
+   <h3>
+    <Link href={'/products/2'}>Product 2</Link>
+   </h3>
+   <h3>
+    <Link href={'/products/3'} replace>
+     {' '}
+     {/* replace clears a history entry Product 3 . */}
+    </Link>
+   </h3>
+   <h3>
+    <Link href={`/products/${productId}`}>Product {productId}</Link>
+   </h3>
   </>
  );
 }
