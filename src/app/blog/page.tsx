375699a57ad1d0152858a41c079aa765d9b3a848
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-07 14:50:13 +0000

    SEO metadata in routing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+ title: {
+  absolute: 'My blog',
+ },
+};
 export default function Blog() {
  return <h2>My blog</h2>;
 }
