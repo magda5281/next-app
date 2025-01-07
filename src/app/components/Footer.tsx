83ecef4e1d920d9be64d488694ce4f9d41bfac9e
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-07 13:40:30 +0000

    Add header and footer into layout

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function footer() {
+ return (
+  <footer style={{ backgroundColor: 'lightblue', padding: '1rem' }}>
+   <p>Footer</p>
+  </footer>
+ );
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+ return (
+  <header style={{ backgroundColor: 'lightblue', padding: '1rem' }}>
+   <p>Header</p>
+  </header>
+ );
+}
