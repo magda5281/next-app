51a3e51c574d6971a63b753476633a31d9809230
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-07 14:18:47 +0000

    Add generateMetadata to dynamic route for dynamic generation of metadata for the page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Metadata } from 'next';
 
-export default function ProductDetails({
+export const generateMetadata = async ({
  params,
-}: {
- params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+ const title = await new Promise((resolve) => {
+  setTimeout(() => {
+   resolve(`iphone ${params?.productId}`);
+  }, 100);
+ });
+ return {
+  title: `Product ${title}`,
+ };
+};
+
+type Props = { params: { productId: string } };
+
+export default function ProductDetails({ params }: Props) {
  return <h2>Product Details for:{params?.productId}</h2>;
 }
